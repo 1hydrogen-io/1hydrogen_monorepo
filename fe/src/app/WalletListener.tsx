e9652667ef6ca7301243cb95f337324597ffbdf6
Vien Le <levienit@gmail.com>
2024-03-22 17:34:33 +0700

    hot fix

--- fe/src/app/WalletListener.tsx
+++ fe/src/app/WalletListener.tsx
@@ -1,48 +1,99 @@
-'use client'
-import useProcessing from '@/lib/hooks/useProcessing';
-import useRefetchBalance from '@/lib/hooks/useRefetchBalance';
-import { fetchWalletInfoGlobalAction } from '@/lib/reduxs/globals/global.action';
-import { useAppDispatch } from '@/lib/reduxs/hooks';
-import {getPackageAction, getUsdbPackageAction} from '@/lib/reduxs/hs-stakings/hs-staking.actions';
-import { useCallback, useEffect } from 'react'
-import { useAccount } from 'wagmi';
+"use client";
+import { getWalletPointApi, signMessageWallet } from "@/lib/apis/account.api";
+import useProcessing from "@/lib/hooks/useProcessing";
+import useRefetchBalance from "@/lib/hooks/useRefetchBalance";
+import useToastCustom from "@/lib/hooks/useToastCustom";
+import { fetchWalletInfoGlobalAction } from "@/lib/reduxs/globals/global.action";
+import { useGlobalState } from "@/lib/reduxs/globals/global.hook";
+import { useAppDispatch, useAppSelector } from "@/lib/reduxs/hooks";
+import {
+  getPackageAction,
+  getUsdbPackageAction,
+} from "@/lib/reduxs/hs-stakings/hs-staking.actions";
+import { useCallback, useEffect } from "react";
+import { useAccount, useSignMessage } from "wagmi";
 
 export default function WalletListener() {
-  const {isConnected, address} = useAccount();
-  const {onOpenProcessing, onCloseProcessing} = useProcessing();
-  const {onFetchVaulStakedInfo, onFetchSTotalStaked, onFetchUsdbVaulStakedInfo, onFetchUsdbSTotalStaked} = useRefetchBalance();
+  const {
+    globalState: { joinCode },
+    onSetJoinCode,
+  } = useGlobalState();
+
+  const { signMessageAsync } = useSignMessage();
+
+  const { isConnected, address } = useAccount();
+  const { onOpenProcessing, onCloseProcessing } = useProcessing();
+  const {
+    onFetchVaulStakedInfo,
+    onFetchSTotalStaked,
+    onFetchUsdbVaulStakedInfo,
+    onFetchUsdbSTotalStaked,
+    onRefetchWalletPointAndCode,
+  } = useRefetchBalance();
 
   const dispatch = useAppDispatch();
+  const { onSuccessToast, onErrorToast } = useToastCustom();
 
-  const onListeningChang = useCallback(async() => {
+  const signMessageWithJoinCode = async () => {
+    try {
+      if (!isConnected || !address) {
+        return;
+      }
+      const yourPoint = await getWalletPointApi(address);
+      if (!Boolean(joinCode) && Boolean(yourPoint.referralCode)) {
+        return;
+      }
+
+      const message = JSON.stringify({
+        wallet: address,
+        joinCode,
+      });
+
+      const result = await signMessageAsync({
+        message,
+      });
+
+      if (result) {
+        await signMessageWallet(address as string, joinCode, result);
+        onSuccessToast("Sign message successfully!");
+        await onRefetchWalletPointAndCode();
+        onSetJoinCode("");
+      }
+    } catch (error: any) {
+      onErrorToast(error?.message || "Failed to sign message!");
+    }
+  };
+
+  const onListeningChang = useCallback(async () => {
     onOpenProcessing();
     try {
+      await signMessageWithJoinCode();
+
       await dispatch(fetchWalletInfoGlobalAction()).unwrap();
+
       onFetchVaulStakedInfo();
       onFetchUsdbVaulStakedInfo();
-    } catch(ex) {}
+    } catch (ex) {}
     onCloseProcessing();
-  }, [isConnected, address])
+  }, [isConnected, address]);
 
-  const getPackage = useCallback(async() => {
-    dispatch(getPackageAction())
-    dispatch(getUsdbPackageAction())
+  const getPackage = useCallback(async () => {
+    dispatch(getPackageAction());
+    dispatch(getUsdbPackageAction());
   }, []);
 
   useEffect(() => {
-    onListeningChang()
+    onListeningChang();
   }, [onListeningChang]);
 
   useEffect(() => {
     getPackage();
-  },[]);
+  }, []);
 
   useEffect(() => {
     onFetchSTotalStaked();
     onFetchUsdbSTotalStaked();
-  },[]);
-
-
+  }, []);
 
   return null;
 }
--- fe/src/ui/components/ConnectWalletButton.tsx
+++ fe/src/ui/components/ConnectWalletButton.tsx
@@ -1,50 +1,14 @@
-'use client'
-import { Flex, Text } from '@chakra-ui/react'
-import React, { useEffect } from 'react'
-import { useConnectModal } from '@rainbow-me/rainbowkit';
-import { useAccount, useSignMessage } from 'wagmi';
-import { showSortAddress } from '@/lib/utls';
-import { plus_jakarta_san } from '@/themes';
-import { useGlobalState } from '@/lib/reduxs/globals/global.hook';
-import { signMessageWallet } from '@/lib/apis/account.api';
-import useToastCustom from '@/lib/hooks/useToastCustom';
-import { useAppSelector } from '@/lib/reduxs/hooks';
-import useRefetchBalance from "@/lib/hooks/useRefetchBalance";
+"use client";
+import { Flex, Text } from "@chakra-ui/react";
+import React from "react";
+import { useConnectModal } from "@rainbow-me/rainbowkit";
+import { useAccount } from "wagmi";
+import { showSortAddress } from "@/lib/utls";
+import { plus_jakarta_san } from "@/themes";
 
 export default function ConnectWalletButton() {
   const { openConnectModal } = useConnectModal();
   const { isConnected, address } = useAccount();
-  const { signMessageAsync } = useSignMessage();
-  const { globalState: { joinCode }, onSetJoinCode } = useGlobalState();
-  const { onSuccessToast, onErrorToast } = useToastCustom();
-  const { balance: { point } } = useAppSelector(p => p.wallet);
-  const { onRefetchWalletPointAndCode } = useRefetchBalance();
-
-  const signMessageWithJoinCode = async () => {
-    try {
-      const message = JSON.stringify({
-        wallet: address,
-        joinCode
-      })
-      const result = await signMessageAsync({
-        message
-      });
-      if (result) {
-        await signMessageWallet(address as string, joinCode, result)
-        onSuccessToast('Sign message successfully!')
-        await onRefetchWalletPointAndCode()
-        onSetJoinCode('')
-      }
-    } catch (error: any) {
-      onErrorToast(error?.message || 'Failed to sign message!')
-    }
-  }
-
-  useEffect(() => {
-    if (isConnected && address && joinCode) {
-      signMessageWithJoinCode().then()
-    }
-  }, [isConnected, address, joinCode]);
 
   const handleOpenConnectWallet = () => {
     openConnectModal && openConnectModal();
@@ -76,19 +40,23 @@ export default function ConnectWalletButton() {
 
   return (
     <Flex
-      p='6px 21px'
-      borderRadius='full'
-      gap='8px' cursor='pointer'
-      bgColor='white'
+      p="6px 21px"
+      borderRadius="full"
+      gap="8px"
+      cursor="pointer"
+      bgColor="white"
       onClick={handleOpenConnectWallet}
     >
-      <Text color='#030D20'
+      <Text
+        color="#030D20"
         fontFamily={plus_jakarta_san.style.fontFamily}
-        fontWeight='800'
-        fontSize='10px'
-        lineHeight='150%'
-        textTransform='uppercase'
-      >Connect Wallet</Text>
+        fontWeight="800"
+        fontSize="10px"
+        lineHeight="150%"
+        textTransform="uppercase"
+      >
+        Connect Wallet
+      </Text>
     </Flex>
-  )
+  );
 }
